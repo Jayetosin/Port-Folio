eb558a0cdaa266f7bed09764c48383f370353827
Jayetosin <oluwatosinjayeola0@gmail.com>
2023-07-13 23:49:41 +0100

    product

--- portfolio/src/Component/Works.jsx
+++ portfolio/src/Component/Works.jsx
@@ -5,7 +5,7 @@ const Works = () => {
     <div>
         <div className='about'>
             <p>Cash 2go Loan App: <a href='https://cash-loan-pq2b6ww1a-joyibeh93.vercel.app'>Loan app</a></p>
-            <p>Netflix Clone: <a href='https://jayetosin.github.io/Netflix-clone/'>Netflix Clone</a></p>
+            <p>Netflix Clone: <a href='https://famous-swan-e1a6f9.netlify.app/'>Netflix Clone</a></p>
             <p>Google Clone:</p>
             <p>Udemy Clone:<a href='https://jayetosin.github.io/Udemy-clone/'>Udemy Clone</a></p>
         </div>
